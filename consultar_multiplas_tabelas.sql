2a5bd4f02b2e956df55bc3393ae20cec2dca8013
Matheus Henrique <matheushenriquedasilvaa.2021@gmail.com>
2022-03-04 23:47:44 -0300

    join

---
+++ consultar_multiplas_tabelas.sql
@@ -0,0 +1,12 @@
+select 
+	e.nome as Estado, 
+	c.nome as Cidade, 
+	regiao as Região 
+from estados e, cidades c
+where e.id = c.estado_id;
+
+select 
+	e.nome as Estado, 
+	c.nome as Cidade, 
+	regiao as Região 
+from estados e inner join cidades c on e.id = c.estado_id
